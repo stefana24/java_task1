7f5fa0fb5439cf304cce5c9454cd150ca5d7187f
stefana24 <43245510+stefana24@users.noreply.github.com>
2023-05-01 18:35:21 +0300

    Add files via upload
    code formatting + replacing dots and commas with space for the whole text

--- src/main/java/org/example/MostCommon.java
+++ src/main/java/org/example/MostCommon.java
@@ -2,29 +2,43 @@ package org.example;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
-
+import java.nio.charset.Charset;
 public class MostCommon {
+    private String readFile(String path, Charset encoding) throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
     public HashMap<String, Integer> getResult(String fileName) {
         HashMap<String, Integer> wordsMap = new HashMap<>();
+        String content = null;
         try {
-            File file = new File(fileName);
-            Scanner input = new Scanner(file);
-            while (input.hasNext()) {
-                String word = input.next().toLowerCase();
-                word = word.replace(".", "");
-                word = word.replace(",", "");
-                if (wordsMap.containsKey(word)) {
-                    wordsMap.put(word, wordsMap.get(word) + 1);
-                } else {
-                    wordsMap.put(word, 1);
-                }
-            }
+            content = this.readFile(fileName, StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        wordsMap.entrySet().stream().sorted((one, another) -> -one.getValue().compareTo(another.getValue())) //sort entries to reverse order
-                .limit(10).forEach(entry -> System.out.println(entry.getKey() + " : " + entry.getValue()));
+        content = content.replace("[\\.,]", "");
+        String[] words = content.split(" ");
+        for(String word: words){
+            if (wordsMap.containsKey(word)) {
+                wordsMap.put(word, wordsMap.get(word) + 1);
+            } else {
+                if(!word.isEmpty()){
+                    wordsMap.put(word, 1);
+                }
+            }
+        }
+        wordsMap.entrySet()
+                .stream()
+                .sorted((one, another) -> -one.getValue()
+                        .compareTo(another.getValue()))
+                .limit(10)
+                .forEach(entry -> System.out.println(entry.getKey() + " : " + entry.getValue()));
         return wordsMap;
     }
 
